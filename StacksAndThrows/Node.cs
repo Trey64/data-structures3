d5884a71b63ee25174f410fc7db49cf82020f5e0
Thomas Reyes <thomasellison.r@gmail.com>
2017-09-26 23:39:53 -0700

    challenge finished and readme updated

---
+++ StacksAndThrows/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues
+{
+    class Node
+    {
+        public object Data { get; set; }
+        public Node Prev { get; set; }
+        public Node Next { get; set; }
+
+    }
+}
---
+++ StacksAndThrows/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StacksAndQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Queue:");
+            Queue testQ = new Queue();
+            testQ.Enqueue("1");
+            testQ.Enqueue("2");
+            testQ.Enqueue("3");
+            testQ.Enqueue("4");
+            testQ.Dequeue();
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Stack:");
+            Stack testS = new Stack();
+            testS.Push("1");
+            testS.Push("2");
+            testS.Push("3");
+            testS.Push("4");
+            testS.Pop();
+
+            Console.Read();
+        }
+    }
+}
