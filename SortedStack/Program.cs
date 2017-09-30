a48a6ff6ade5950ab00bb1d1861a620de911a6c4
Thomas Reyes <thomasellison.r@gmail.com>
2017-09-29 17:18:49 -0700

    Challenge finished, readme updated, whiteboard included

---
+++ SortedStack/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SortedStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stack:");
+            Stack testS = new Stack();
+            testS.Push("o");
+            testS.Push("x");
+            testS.Push("d");
+            testS.Push("a");
+            testS.Push("z");
+            testS.Pop();
+
+            Console.WriteLine("");
+            Console.WriteLine("Sorted:");
+            testS.SortPush("k");
+
+
+            Console.Read();
+        }
+    }
+}
