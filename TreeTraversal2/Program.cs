c85e90820909b6b54b0617445c3b3d321c5a66d5
Thomas Reyes <thomasellison.r@gmail.com>
2017-10-04 16:50:22 -0700

    Challenge finished, readme updated, whiteboard included

---
+++ TreeTraversal2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TreeTraversal2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PostOrder Traversal \n");
+
+            Tree testTree = new Tree(1);
+
+            testTree.Left = new Tree(2);
+            testTree.Left.Left = new Tree(3);
+            testTree.Left.Right = new Tree(4);
+            testTree.Right = new Tree(5);
+            testTree.Right.Left = new Tree(6);
+            testTree.Right.Right = new Tree(7);
+
+            testTree.PostOrder();
+
+            Console.WriteLine("");
+            Console.WriteLine("InOrder Traversal \n");
+
+            testTree.InOrder();
+
+            Console.Read();
+        }
+    }
+}
---
+++ TreeTraversal2/Tree.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreeTraversal2
+{
+    public class Tree
+    {
+        public int Data { get; set; }
+        public Tree Left { get; set; }
+        public Tree Right { get; set; }
+
+        public Tree(int data)
+        {
+            Data = data;
+            Left = null;
+            Right = null;
+        }
+
+        public void PostOrder()
+        {
+            if (Right != null)
+            {
+                Right.PostOrder();
+            }
+            if (Left != null)
+            {
+                Left.PostOrder();
+            }
+            Console.WriteLine(Data);
+        }
+
+        public void InOrder()
+        {
+            if (Left != null)
+            {
+                Left.InOrder();
+            }
+            Console.WriteLine(Data);
+            if (Right != null)
+            {
+                Right.InOrder();
+            }
+        }
+
+    }
+}
