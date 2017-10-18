798ed8b064b23d1755fd7fd22b302f3ddb0d5d8c
Thomas Reyes <thomasellison.r@gmail.com>
2017-10-17 17:52:16 -0700

    Challenge finished, readme updated, whiteboard included

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree testTree = new Tree();
+
+            Node root = null;
+
+            root = testTree.Add(root, 2);
+
+            testTree.Add(root, 1);
+            testTree.Add(root, 3);
+
+            Console.Read();
+        }
+    }
+}
---
+++ BinarySearchTree/Tree.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    class Tree
+    {
+        public Node Add(Node node, int data)
+        {
+
+            /* If the tree is empty, return a new node */
+            if (node == null)
+            {
+                return (new Node(data));
+            }
+            else
+            {
+
+                /* Otherwise, recur down the tree */
+                if (data <= node.Data)
+                {
+                    node.Left = Add(node.Left, data);
+                    Console.WriteLine($"Adding node with value of {data}");
+                }
+                else
+                {
+                    node.Right = Add(node.Right, data);
+                    Console.WriteLine($"Adding node with value of {data}");
+                }
+
+                /* return the (unchanged) node pointer */
+                return node;
+            }
+        }
+    }
+}
