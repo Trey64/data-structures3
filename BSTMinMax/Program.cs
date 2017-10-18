ebbf8c45418a84ec5d0136185a2ac897d34ce85d
Thomas Reyes <thomasellison.r@gmail.com>
2017-10-17 17:04:52 -0700

    Challenge finished, successfully finding min and max

---
+++ BSTMinMax/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BSTMinMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree testTree = new Tree();
+
+            Node root = null;
+
+            root = testTree.Add(root, 4);
+
+            testTree.Add(root, 2);
+            testTree.Add(root, 1);
+            testTree.Add(root, 3);
+            testTree.Add(root, 6);
+            testTree.Add(root, 5);
+
+            Console.WriteLine("The minimum value of my tree is " + testTree.Minimum(root));
+            Console.WriteLine("The maximum value of my tree is " + testTree.Maximum(root));
+
+            Console.Read();
+        }
+    }
+}
--- README.md
+++
@@ -1,5 +0,0 @@
-# Breadth First Traversal
-- For this challenge, we utilized various resources.
-- https://www.cs.bu.edu/teaching/c/tree/breadth-first/ - This was our first resource, and it taught us what breadth first traversal is and when we'd use it.
-- https://msdn.microsoft.com/en-us/library/7977ey2c(v=vs.110).aspx - We used this doc to learn about the built-in Queue class and the properties/methods that are available.
-- https://jamesmccaffrey.wordpress.com/2011/10/22/breadth-first-and-depth-first-traversal-of-a-c-binary-search-tree/ - We got our solution from this blog, which uses a queue to implement breadth-first traversal.
