932acc6aaefbc0eee810e6b644d4a61667aa3f59
Thomas Reyes <thomasellison.r@gmail.com>
2017-11-02 01:36:16 -0700

    challenge attempted, readme updated, whiteboard included

---
+++ Matrix/Matrix/Matrix/MxNMatrix.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matrix
+{
+    class MxNMatrix
+    {
+        int[,] matrixArray = new int[2, 2]
+        {
+                { 2, 5 },
+                { 8, 0 },
+        };
+
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+
+        public MxNMatrix(int rows, int columns)
+        {
+            matrixArray = new int[rows, columns];
+            Rows = rows;
+            Columns = columns;
+        }
+
+        public void FindZero(int x, int y, int value)
+        {
+            if (value == 0)
+            {
+                for (int i = 0; i < Rows; i++)
+                {
+                    matrixArray[i, y] = 0;
+                }
+
+                for (int j = 0; j < Columns; j++)
+                {
+                    matrixArray[x, j] = 0;
+                }
+            }
+            else
+            {
+                matrixArray[x, y] = value;
+            }
+        }
+    }
+}
---
+++ Matrix/README.md
@@ -0,0 +1,5 @@
+Going into this challenge, I was unfamiliar with multidimensional arrays. We looked at the resources below to learn more, as well as some youtube videos. For our implementation, first we wanted to find the element containing zero. Then we want to change everything in the same index to zero. We tried to start with a small 2x2 matrix to make it simpler, but our solution isn't working so we will have to try a different approach.
+
+Resources used:
+https://www.dotnetperls.com/2d
+http://answers.unity3d.com/questions/445116/how-to-check-if-a-value-exists-in-an-array-c.html
