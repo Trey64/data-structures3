5749bd6502cb596d1416c1c6bd7fe0060567deab
Thomas Reyes <thomasellison.r@gmail.com>
2017-11-06 18:06:00 -0800

    challenge finished, readme updated, whiteboard added

---
+++ MiscAlgorithms/TowersOfHanoi/TowersOfHanoi/Towers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TowersOfHanoi
+{
+    class Towers
+    {
+        public void MoveTower(int discs, string start, string aux, string end)
+        {
+            if (discs == 1)
+            {
+                Console.WriteLine(start + " -> " + end);
+            }
+            if (discs > 1)
+            {
+                MoveTower(discs - 1, start, end, aux);
+                Console.WriteLine(start + " -> " + end);
+                MoveTower(discs - 1, aux, start, end);
+            }
+        }
+    }
+}
